7e9776e68fe70c3afd1121d0b63a0e6c5734ba0e
Jack Mitchell <jack@communistcode.co.uk>
2011-09-02 13:10:53 +0100

    add space in smallLoginInfo

--- includes/smallLoginInfo.inc.php
+++ includes/smallLoginInfo.inc.php
@@ -5,6 +5,7 @@
 	$member = unserialize($_SESSION['member']);
 
 	echo("Logged in as ".$member->getUsername()."<br />");
+	echo("<br />");
 	echo("<a href='".$directoryPath."/membership/index.php'>Members Area</a><br />");	
 	echo("<a href='".$directoryPath."/membership/logout.php'>Logout</a>");
 
