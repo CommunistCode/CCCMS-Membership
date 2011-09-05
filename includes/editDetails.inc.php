203c5d164e25668b0c00ccba04a3aee20afa3b73
Jack Mitchell <jack@communistcode.co.uk>
2011-09-05 11:59:23 +0100

    editDetails add table headers

--- includes/editDetails.inc.php
+++ includes/editDetails.inc.php
@@ -1,11 +1,11 @@
 <form method='post' action='editDetails.php'>
 	<table>
 		<tr>
-			<td width='150'>Location</td>
+			<th width='150'>Location</th>
 			<td><input type='text' name='location' value='<?php echo($member->getLocation()); ?>' /></td>
 		</tr>
 		<tr>
-			<td>Email</td>
+			<th>Email</th>
 			<td><input type='text' name='email' value='<?php echo($member->getEmail()); ?>' /></td>
 		</tr>
 		<tr>
@@ -18,15 +18,15 @@
 
 	<table>
 		<tr>
-			<td width='200'>Current Password</td>
+			<th width='200'>Current Password</th>
 			<td><input type='text' name='currentPassword' /></td>
 		</tr>
 		<tr>
-			<td>New Password</td>
+			<th>New Password</th>
 			<td><input type='text' name='newPass' /></td>
 		</tr>
 		<tr>
-			<td>Confirm Password</td>
+			<th>Confirm Password</th>
 			<td><input type='text' name='confirmPass' /></td>
 		</tr>
 		<tr>
